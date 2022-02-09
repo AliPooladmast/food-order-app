9c4cdd7a1c4ece4237e3866d7298bff47c08f4ac
AliPooladmast <ali.pooladmast.gh@gmail.com>
2022-02-09 17:52:43 +0330

    ommiting the 1x image quailiy from main page

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -2,14 +2,14 @@ import { Fragment } from "react";
 
 import HeaderCartButton from "./HeaderCartButton";
 import classes from "../../Styles/Header.module.css";
-import mealsImage1x from "../../Assets/Images/meals@1x.jpg";
+// import mealsImage1x from "../../Assets/Images/meals@1x.jpg";
+// import mealsImageWeb1x from "../../Assets/Images/meals@1x.webp";
 import mealsImage2x from "../../Assets/Images/meals@2x.jpg";
-import mealsImageWeb1x from "../../Assets/Images/meals@1x.webp";
 import mealsImageWeb2x from "../../Assets/Images/meals@2x.webp";
 
 const Header = (props) => {
-  const mealsImageWeb = `${mealsImageWeb1x} 1x, ${mealsImageWeb2x} 2x`;
-  const mealsImage = `${mealsImage1x} 1x, ${mealsImage2x} 2x`;
+  // const mealsImageWeb = `${mealsImageWeb1x} 1x, ${mealsImageWeb2x} 2x`;
+  // const mealsImage = `${mealsImage1x} 1x, ${mealsImage2x} 2x`;
 
   return (
     <Fragment>
@@ -19,9 +19,9 @@ const Header = (props) => {
       </header>
       <div className={classes["main-image"]}>
         <picture>
-          <source type="image/webp" srcset={mealsImageWeb} />
-          <source type="image/jpg" srcset={mealsImage} />
-          <img src={mealsImage1x} alt="A table full of delicious food!" />
+          <source type="image/webp" srcset={mealsImageWeb2x} />
+          <source type="image/jpg" srcset={mealsImage2x} />
+          <img src={mealsImage2x} alt="A table full of delicious food!" />
         </picture>
       </div>
     </Fragment>
